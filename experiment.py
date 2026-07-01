8fc720aac91f5b2aec19d8677e39d7bea7323e4c
Rice-03 <raeezahmed03@gmail.com>
2026-07-02 00:38:28 +0200

    added comments to both files, and fixed table results and formatting.

--- experiment.py
+++ experiment.py
@@ -5,34 +5,44 @@ from csp_data import(
 import os
 from solver_advanced import S_backtracking, mrv_backtracking, forward_checking
 
-SCENARIOS = [ ("Small Instance", SMALL_INSTANCE, SMALL_OVERLAPS, None, None),
-              ("Full Instance", EXAMS, OVERLAPS, None, None),
-              ("Extended Instance", EXTENDED_INSTANCE, EXTENDED_OVERLAPS, None, None),
-              ("Over-Constrained Instance",EXAMS, OVERLAPS, OVERCONSTRAINED_INSTANCE, ["HallB"] )]
+#store all the problem scenarios  and search strategies in a list of tuples
+SCENARIOS = [ ("Small", SMALL_INSTANCE, SMALL_OVERLAPS, None, None),
+              ("Full", EXAMS, OVERLAPS, None, None),
+              ("Extended", EXTENDED_INSTANCE, EXTENDED_OVERLAPS, None, None),
+              ("OverConstrained",EXAMS, OVERLAPS, OVERCONSTRAINED_INSTANCE, ["HallB"] )] #overconstrained scenario using limited time slots and venue hallB
 
 SEARCH_STRATS = [("Simple Backtracking", S_backtracking),
                  ("MRV (Minimum Remaining Values)", mrv_backtracking),
                  ("Forward Checking", forward_checking)]
 
+
 if __name__ == "__main__":
-    os.makedirs("results", exist_ok=True)
-    
-    header = f"{"Scenario":<20} {"Simple Backtracking": >12} {"MRV": >12} {"Forward Checking": >12}"
+    os.makedirs("results", exist_ok=True) #created results folder if it doesnt exist
+    #the below code creates a table of the results (different search strats)
+    header = f"{"Scenario":<20} {"Simple Backtracking":>22} {"BT + MRV with Degree":>22} {"BT + Forward Checking":>22}"
     div = "-" * len(header)
     print(header)
     print(div)
-    lines = [header,div]
+    lines = [header, div]
 
-for name, exams, overlaps, time_slots, written_venues in SCENARIOS:
-    cols = []
+for name, exams, overlaps, time_slots, written_venues in SCENARIOS: #loops through each scenario
+    cols = [] #creates a list to store the results for each search strategy
+    #below loops through each search strategy 
     for _, fn in SEARCH_STRATS:
         results, steps = fn(exams, overlaps, time_slots=time_slots, written_venues=written_venues)
-        condition = "Solution Found" if results is not None else "No Solution"
-        cols.append(f"{steps} ({condition})")
-    row = f"{name:<20} {cols[0]:>12} {cols[1]:>12} {cols[2]:>20}"
+        condition = "Solution Found" if results is not None else "No Solution" #checks if a solution was found or not
+        cols.append(f"{steps} ({condition})") #combines the number of steps and if there was a solution and adds it to the table cols list
+    row = f"{name:<20} {cols[0]:>22} {cols[1]:>22} {cols[2]:>22}"
     print(row)
     lines.append(row)
 
+#below code writes the results to a text file in the results folder
 with open("results/results.txt", "w") as f:
     f.write("\n".join(lines) + "\n")
 print("\nResults written to results.txt")
+
+#used to help code :
+#claude for debugging and assisstance in understading how to set up the for loops
+#https://www.w3schools.com/python/python_tuples.asp
+#https://www.geeksforgeeks.org/python/writing-to-file-in-python/
+#https://www.geeksforgeeks.org/python/create-a-directory-in-python/
--- solver_advanced.py
+++ solver_advanced.py
@@ -4,8 +4,10 @@ from csp_data import(
 )
 from solver_basic import is_valid
 
-#Simple Search Strategy: Simple Backtracking
 #The below simple backtracking function is adpated from Russell and Norvig's textbooks pseudocode for backtracking search.
+# as well as the theory for the complex search strategies
+
+#Simple Search Strategy: Simple Backtracking
 #assigned parameter: dictionary of assigned exams and their values
 #domains parameter: dictionary of all exams and their possible values
 #examOrder parameter: list of exams in a fixed order
@@ -61,7 +63,7 @@ def S_backtracking(exams, overlaps, time_slots=None, written_venues=None, log_fn
     result = _backtrack_hlp(assigned, domains, examOrder, counter, overlaps, log_fn)
     return result, counter[0]
 
-#Complex Search Strategy 1: MRV with Degree heuristic + BT
+#Complex Search Strategy 1: Backtracking with MRV heuristic + Degree heuristic
 def _mrv_backtrack_hlp(assigned, domains, counter, overlaps, log_fn=None):
     #if every exam has an assigned value, the timetable is complete
     if len(assigned) == len(domains):
@@ -122,10 +124,11 @@ def mrv_backtracking(exams, overlaps, time_slots=None, written_venues=None, log_
     domains = generate_domains(exams, time_slots=time_slots, written_venues=written_venues)
     counter = [0]
 
+    #calls the above function to find a solution
     result = _mrv_backtrack_hlp(assigned, domains, counter, overlaps, log_fn)
     return result, counter[0]
 
-#Complex Search Strategy 2: Forward Checking
+#Complex Search Strategy 2: Backtracking with Forward Checking
 def _Forward_checking_hlp(assigned, currentDomains, examOrder, counter, overlaps, log_fn=None):
      #if every exam has an assigned value, the timetable is complete
     if len(assigned) == len(examOrder):
@@ -134,37 +137,43 @@ def _Forward_checking_hlp(assigned, currentDomains, examOrder, counter, overlaps
     #gets the next exam in line thats not been assigned.
     examCurrent = examOrder[len(assigned)]
 
-    
+    #try every possible value in the current exam's domains
     for value in list(currentDomains[examCurrent]):
         counter[0] += 1
 
+        #uses is_valid function from solver_basic to check if the chosen value has any clashes
         if is_valid(examCurrent, value, assigned, overlaps):
             assigned[examCurrent] = value
             if log_fn:
                 log_fn({"exam": examCurrent, "value": str(value),
                         "step": counter[0], "status": "success"})
 
-            savedDomains = {}
-            DeadEnd = False
+            savedDomains = {} #backup of the domains so that we can comeback after pruning 
+            DeadEnd = False #tracks when an exam runs out of values
+
+            #goes through every unassigned exam 
             for other in [exam for exam in examOrder if exam not in assigned]:
-                savedDomains[other] = list(currentDomains[other])
+                savedDomains[other] = list(currentDomains[other]) #saves current domain for backtracking purposes
+                #keeps legal values only
                 currentDomains[other] = [v for v in currentDomains[other] if is_valid(other, v, assigned, overlaps)]
 
+                #if this exam has no legal options left, mark this value as a deadend to cancel the recursion
                 if not currentDomains[other]:
                     DeadEnd = True
                     break
-
+            #only go further if exam domain is not empty
             if not DeadEnd:
                 result = _Forward_checking_hlp(assigned, currentDomains, examOrder, counter, overlaps, log_fn)
                 if result is not None:
-                    return result
+                    return result #solution found
 
             if log_fn:
                 log_fn({"exam": examCurrent, "value": str(value),
                         "step": counter[0], "status": "backtrack"})
+            #put the saved domains back so a new value can start from the prevosuly saved domains
             for other, domain in savedDomains.items():
                 currentDomains[other] = domain
-
+            #delete the exam's current assignment
             del assigned[examCurrent]
         else:
             if log_fn:
@@ -180,5 +189,13 @@ def forward_checking(exams, overlaps, time_slots=None, written_venues=None, log_
     examOrder = list(exams.keys())
     counter = [0]
 
+    #calls the above function to find a solution
     result = _Forward_checking_hlp(assigned, currentDomains, examOrder, counter, overlaps, log_fn)
     return result, counter[0]
+
+#used to help code and for understanding::
+#the russell and norvig textbook
+#https://github.com/aimacode/aima-python/blob/master/aima/csp.py
+#https://simpleai.readthedocs.io/en/latest/_modules/simpleai/search/csp.html
+#https://www.cs.cmu.edu/~15281/coursenotes/constraints/
+#Claude for debugging and to help understand MRV/forward checking
